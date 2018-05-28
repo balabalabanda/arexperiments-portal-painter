4964f05c400cd34261c72365ad3b72baad13c51c
balabalabanda <x384384384@live.com>
2018-05-28 20:40:28 +0800

    fix ARcore 1.2

--- Assets/Scripts/RaycastPixel.cs
+++ Assets/Scripts/RaycastPixel.cs
@@ -14,12 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.Rendering;
 using GoogleARCore;
 using UnityEngine;
-using GoogleARCore.HelloAR;
 
 public class RaycastPixel : MonoBehaviour
 {
